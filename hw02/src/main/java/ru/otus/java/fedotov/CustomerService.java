22ca1fc6e13e0c71f83482ebea4e1206d80ba280
FedotovDmitry1997 <dimbaikal@gmail.com>
2024-01-18 22:34:09 +0300

    дз сделано

--- hw02/src/main/java/ru/otus/java/fedotov/CustomerService.java
+++ hw02/src/main/java/ru/otus/java/fedotov/CustomerService.java
@@ -1,21 +1,44 @@
 package ru.otus.java.fedotov;
 
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 @SuppressWarnings({"java:S1186", "java:S1135", "java:S1172"}) // при выполнении ДЗ эту аннотацию надо удалить
 public class CustomerService {
 
     // todo: 3. надо реализовать методы этого класса
     // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private final NavigableMap<Customer, String> customerMap = new TreeMap<>();
 
     public Map.Entry<Customer, String> getSmallest() {
         // Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> nextCustomer = customerMap.firstEntry();
+        if (nextCustomer == null) {
+            return null;
+        }
+        return Map.entry(
+                new Customer(
+                        nextCustomer.getKey().getId(),
+                        nextCustomer.getKey().getName(),
+                        nextCustomer.getKey().getScores()),
+                nextCustomer.getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> nextCustomer = customerMap.higherEntry(customer);
+        if (nextCustomer == null) {
+            return null;
+        }
+        return Map.entry(
+                new Customer(
+                        nextCustomer.getKey().getId(),
+                        nextCustomer.getKey().getName(),
+                        nextCustomer.getKey().getScores()),
+                nextCustomer.getValue());
     }
 
-    public void add(Customer customer, String data) {}
+    public void add(Customer customer, String data) {
+        customerMap.put(customer, data);
+    }
 }
--- hw02/src/test/java/CustomerTest.java
+++ hw02/src/test/java/CustomerTest.java
@@ -2,7 +2,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.otus.java.fedotov.Customer;
@@ -29,7 +28,6 @@ class CustomerTest {
     }
 
     @Test
-    @Disabled("надо удалить") // надо удалить
     @DisplayName("Объект Customer как ключ в карте")
     void customerAsKeyTest() {
         // given
@@ -57,7 +55,6 @@ class CustomerTest {
     }
 
     @Test
-    @Disabled("надо удалить") // надо удалить
     @DisplayName("Сортировка по полю score, итерация по возрастанию")
     void scoreSortingTest() {
         // given
@@ -97,7 +94,6 @@ class CustomerTest {
     }
 
     @Test
-    @Disabled("надо удалить") // надо удалить
     @DisplayName("Модификация коллекции")
     void mutationTest() {
         // given
@@ -119,7 +115,6 @@ class CustomerTest {
     }
 
     @Test
-    @Disabled("надо удалить") // надо удалить
     @DisplayName("Возвращание в обратном порядке")
     void reverseOrderTest() {
         // given
